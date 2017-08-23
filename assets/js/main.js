04ef1020df6dc0c4ff05c3ff5af3b18fab4bb668
prabhat22 <prabhatlokwani5@gmail.com>
2017-08-23 20:20:26 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function()
+                 {
+    new WOW().init();
+     $("#nav_7").click(function(){
+            $("#lgn").toggle(1000);
+        })
+    $("#BTN").click(function()
+                   {
+        $("#main-nav").toggle(1);
+        
+    })
+
+    $("#sop").click(function()
+                   {
+        $("#desrt").hide(1000);
+         $("#dinnr").hide(1000);
+                
+ 
+        $("#soup").show(1000);
+        
+    })
+       $("#dnr").click(function()
+                   {
+        $("#desrt").hide(1000);
+         $("#soup").hide(1000);
+        $("#dinnr").show(1000);
+        
+    })
+    
+       $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 60000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+   $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:false,
+       autoplay:true,
+       
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        })
+
+    
+  
+})
+     
+
+
+
+
+
+
+
+
